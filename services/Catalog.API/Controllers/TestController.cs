364345350b83d8a63b23befe8cc3ba1785cb104f
Igor Flyunt <igor.flyunt@globallogic.com>
2022-09-18 10:40:52 +0300

    Added dockercompose for catalog.api and added new basket project

---
+++ services/Catalog.API/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World works!");
+    }
+}
